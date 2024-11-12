a56bd64267f5e96d3defcfc8fe6effb485eb87a9
acleda <metythorn@gmail.com>
2024-11-12 11:21:08 +0700

    feat: update tbl

--- app/main.py
+++ app/main.py
@@ -12,6 +12,14 @@ SUPABASE_KEY: Optional[str] = os.getenv('SUPABASE_KEY')
 # create supabase client
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
+# insert a new row into table 
+# new_row = {'first_name': 'xyber'}
+# supabase.table('demo-table').insert(new_row).execute()
+
+# update table
+new_row = {'first_name': 'punk'}
+supabase.table('demo-table').update(new_row).eq('id', 2).execute()
+
 # execute query
 results = supabase.table('demo-table').select('*').execute() 
 
