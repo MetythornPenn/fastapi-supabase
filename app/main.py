9478c629a2244fedccded6aef480d4d211f81975
acleda <metythorn@gmail.com>
2024-11-12 11:27:53 +0700

    feat: delete & fetch all

--- app/main.py
+++ app/main.py
@@ -12,15 +12,19 @@ SUPABASE_KEY: Optional[str] = os.getenv('SUPABASE_KEY')
 # create supabase client
 supabase: Client = create_client(SUPABASE_URL, SUPABASE_KEY)
 
-# insert a new row into table 
+"""--- insert a record into table ---"""
 # new_row = {'first_name': 'xyber'}
 # supabase.table('demo-table').insert(new_row).execute()
 
-# update table
-new_row = {'first_name': 'punk'}
-supabase.table('demo-table').update(new_row).eq('id', 2).execute()
+"""---update record---"""
+# new_row = {'first_name': 'punk'}
+# supabase.table('demo-table').update(new_row).eq('id', 2).execute()
 
-# execute query
+"""---delete record"""
+supabase.table('demo-table').delete().eq('id', 4).execute()
+
+
+"""--- fetch all records---"""
 results = supabase.table('demo-table').select('*').execute() 
 
 print(results)
